6a87818266b45e5d4600933f82ff286c88528e1c
harikeshjaiswal <104246678+FenilAGhelani@users.noreply.github.com>
2024-12-19 17:29:17 +0530

    Update AuthPage.jsx

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -17,7 +17,6 @@ const AuthPage = () => {
 						<Box textAlign={"center"}>Get the app.</Box>
 						<Flex gap={5} justifyContent={"center"}>
 							<Image src='/playstore.png' h={"10"} alt='Playstore logo' />
-							<Image src='/microsoft.png' h={"10"} alt='Microsoft logo' />
 						</Flex>
 					</VStack>
 				</Flex>
